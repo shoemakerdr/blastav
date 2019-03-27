66de84c537853ba22d198bcf11ee48910adb29c5
Derek Shoemaker <shoemakerdr@gmail.com>
2019-03-27 00:05:59 -0500

    Updates

---
+++ precache-manifest.b0c8897ce38747a59637a10f7c473490.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "232fcf951858c5a9b260",
+    "url": "/blastav/static/css/main.d112d8f1.chunk.css"
+  },
+  {
+    "revision": "232fcf951858c5a9b260",
+    "url": "/blastav/static/js/main.802be102.chunk.js"
+  },
+  {
+    "revision": "56e5e78d91fcadff3422",
+    "url": "/blastav/static/js/runtime~main.23a04c32.js"
+  },
+  {
+    "revision": "ba3860812ca5f0cc104c",
+    "url": "/blastav/static/js/2.0a5cbac6.chunk.js"
+  },
+  {
+    "revision": "0594ec42fc6134ac3a082e92d3652332",
+    "url": "/blastav/static/media/blastav-rocket.0594ec42.png"
+  },
+  {
+    "revision": "c7327da80b07c65007c7f28f06091f6a",
+    "url": "/blastav/static/media/blastav-logo.c7327da8.svg"
+  },
+  {
+    "revision": "b01b7b404b9ab8cdde9e43f6b53e735e",
+    "url": "/blastav/static/media/pat-sound-board.b01b7b40.jpg"
+  },
+  {
+    "revision": "0ac848ba00b0407506982280dd67e3e6",
+    "url": "/blastav/index.html"
+  }
+];
